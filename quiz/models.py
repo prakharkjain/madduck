2b3b0ae0f5febc1414fe6e3ec4e1354308b76978
chaitanya <gopi.daiict@gmail.com>
2010-03-20 14:29:35 +0530

    changed the create-a-quiz text fields to dropdowns.

--- quiz/models.py
+++ quiz/models.py
@@ -12,12 +12,37 @@ from django.db.models.base import get_absolute_url
 # Create your models here.
 class Quiz(models.Model):
     """ Quiz Model: name, subject, date, class, total_marks, total_questions, duration, note """
-    classs = models.CharField(_("Class"), max_length=255) 
-    subject = models.CharField(_("Subject"), max_length=255)
+    
+    """ @TODO these should not be hardcoded.. """
+    CLASS_CHOICES = (
+        (u"7", "7th Standard"),
+        (u"8", "8th Standard"),
+        (u"9", "9th Standard"),
+        (u"10", "10th Standard"),
+    )
+    
+    """ @TODO these should not be hardcoded.. """
+    SUBJECT_CHOICES = (
+        (u"ENGLISH", u"English"),
+        (u"HISTORY", u"History"),
+        (u"GEOGRAPHY", u"Geography"),
+        (u"MATHS", u"Mathematics")
+    )
+    
+    """ @TODO these should not be hardcoded.. """
+    DURATION_CHOICES = (
+        (30, u"30 minutes"),
+        (45, u"45 minutes"),
+        (60, u"60 minutes"),
+        (90, u"1 hour, 30 minutes.")
+    )
+
+    classs = models.CharField(_("Class"), max_length=15, choices=CLASS_CHOICES, default=_("select a class")) 
+    subject = models.CharField(_("Subject"), max_length=15, choices=SUBJECT_CHOICES, default=_("select a subject"))
     
     total_marks = models.IntegerField(_("Total Marks"))
     total_questions = models.IntegerField(_("Total Questions"))
-#    duration = models.TimeField()
+    duration = models.IntegerField(_("Duration Of The Test"), choices=DURATION_CHOICES, default=_("select test duration"))
     
     name = models.CharField(_("Quiz Name"), max_length=500)
     note = models.CharField(_("Quiz Note"), max_length=500)
