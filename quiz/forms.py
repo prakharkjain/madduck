eb55b90e9a0e3637af99d292c0797e472fd5569f
chaitanya <gopi.daiict@gmail.com>
2010-03-20 11:47:32 +0530

    intermediate commit; code clean up;

--- quiz/forms.py
+++ quiz/forms.py
@@ -6,24 +6,24 @@ from django.utils.translation import ugettext_lazy as _
 from quiz.models import Quiz, Question
 
 class QuizForm(forms.ModelForm):
-	""" 
-	Quiz Form : form associated to the quiz model
-	"""
-	def __init__ (self, *args, **kwargs):
-		super(QuizForm, self).__init__(*args, **kwargs)
+    """ 
+    Quiz Form : form associated to the quiz model
+    """
+    def __init__ (self, *args, **kwargs):
+        super(QuizForm, self).__init__(*args, **kwargs)
 
-	def clean(self):
-		""" do validation here. """
-		#name is mandatory
-		if 'name' not in self.cleaned_data:
-			return
-		#if a book with that title already exists
-#		if Quiz.objects.filter(name=self.cleaned_data['name']).count() > 0:
-#			raise forms.ValidationError(_("This quiz already exists"))
-		return self.cleaned_data
+    def clean(self):
+        """ do validation here. """
+        #name is mandatory
+        if 'name' not in self.cleaned_data:
+            return
+        #if a book with that title already exists
+#        if Quiz.objects.filter(name=self.cleaned_data['name']).count() > 0:
+#            raise forms.ValidationError(_("This quiz already exists"))
+        return self.cleaned_data
         
-	class Meta:
-		model = Quiz
+    class Meta:
+        model = Quiz
 
 class QuestionForm(forms.ModelForm):
     """ Question Form : form associated to the question model. """
